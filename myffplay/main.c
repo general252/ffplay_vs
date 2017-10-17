e06c63cee494216abaf07269ae814ef9769bc53b
name <name@mail.com>
2017-10-17 17:20:41 +0800

    -fix: 可运行

--- myffplay/main.c
+++ myffplay/main.c
@@ -1,34 +1,8 @@
-#define inline __inline
-#include <stdio.h>
-#include <math.h>
-#include <inttypes.h>
-#include <limits.h>
-#include <signal.h>
-#include <stdint.h>
-
-#include "libavutil/avstring.h"
-#include "libavutil/eval.h"
-#include "libavutil/mathematics.h"
-#include "libavutil/pixdesc.h"
-#include "libavutil/imgutils.h"
-#include "libavutil/dict.h"
-#include "libavutil/parseutils.h"
-#include "libavutil/samplefmt.h"
-#include "libavutil/avassert.h"
-#include "libavutil/time.h"
-#include "libavformat/avformat.h"
-#include "libavdevice/avdevice.h"
-#include "libswscale/swscale.h"
-#include "libavutil/opt.h"
-#include "libavcodec/avfft.h"
-#include "libswresample/swresample.h"
+#include "play.h"
 
 #include <SDL.h>
 #include <SDL_thread.h>
 #define SDL_main main
-
-#include <assert.h>
-
 
 #pragma comment(lib, "avcodec.lib")
 #pragma comment(lib, "avformat.lib")
@@ -43,7 +17,7 @@
 
 int main(int argc, char *argv[])
 {
-    av_register_all(); //! 注册所有编码器和解码器
+    play("../xibushijie.mp4");
 
     return 0;
 }
---
+++ myffplay/play.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int play(const char* filename);
+
+
+void show_paly_help();
